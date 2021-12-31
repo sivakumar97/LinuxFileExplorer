6d448e78191a801a039e727736bcc37559175dda
Siva Kumar Lakkoju <slakkoju79@gmail.com>
2021-12-31 14:37:36 +0530

    Add files via upload
    Added home, back, file opening features

--- header.h
+++ header.h
@@ -7,21 +7,29 @@
 #include <pwd.h>
 #include <grp.h>
 #include <termios.h>
+#include <unistd.h>
 #include <sys/ioctl.h>
+#include <spawn.h>
+#include <sys/wait.h>
 using namespace std;
 using namespace std::filesystem;
-/*
-For directory operations
-*/
+/*For directory operations*/
+extern vector<directory_entry> dir;
+extern stack<path> backstk;
+extern stack<path> frwdstk;
+extern path q;
+void printDir(path );
+void scrollDir(vector<directory_entry> &, int ,int ,int );
+string perms_linux(perms );
+void print_dirent(directory_entry const& ,bool );
+void updateDirEnt(path , int &, int &, int & );
 
-void printDir(path p);
-void scrollDir(vector<directory_entry> &dir, int first,int last,int curr);
-string perms_linux(perms p);
-void print_dirent(directory_entry const& d,bool color);
-
-/*
-For opening a file in nano
-*/
-
-
+/*For opening a file in nano*/
+extern char **environ;
+extern char *temp;
+extern pid_t pid;
+extern int status;
+extern char* arg[];
+void open_media_pdf(string );
+void open_txt(string );
 #endif // HEADER_H
